80108f14939bda9ace0c20c2d9aeb61609e848b8
Sylwia Prasoł <99748076+frutts@users.noreply.github.com>
2023-06-10 14:06:22 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+
 
 app.get("/test", (_req, res) => {
-
 	res.status(200).send("Hello World")
-})
+
+});
 
 module.exports = app;
